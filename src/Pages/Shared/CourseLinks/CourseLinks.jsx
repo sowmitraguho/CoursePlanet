0f16fec2bb9c1c1bb3bc319341d84a291992cef0
SowmitraGuha <sowmmochy@gmail.com>
2024-04-10 01:18:32 +0600

    category link set

--- src/Pages/Shared/CourseLinks/CourseLinks.jsx
+++ src/Pages/Shared/CourseLinks/CourseLinks.jsx
@@ -1,11 +1,30 @@
-
+import { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
 
 const CourseLinks = () => {
-    return (
-        <div>
-            <h1>All Couses Links Here</h1>
-        </div>
-    );
+  const [categories, setCategories] = useState("");
+
+  useEffect(() => {
+    fetch("http://localhost:8000/categories")
+      .then((res) => res.json())
+      .then((data) => {
+        setCategories(data);
+        console.log(data);
+      });
+  }, []);
+
+  return (
+    <div>
+      <h4>All Categories: {categories.length}</h4>
+      <div>
+        {categories.map((category) => (
+          <p key={category.id}>
+            <Link to={`/category/${category.id}`}>{category.name}</Link>
+          </p>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default CourseLinks;
