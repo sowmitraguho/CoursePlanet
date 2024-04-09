3c93b341a78756ff86636ae4a3c6f59b1514d0d4
SowmitraGuha <sowmmochy@gmail.com>
2024-04-09 17:40:57 +0600

    social media icos set

--- src/Pages/Shared/Header/Header.jsx
+++ src/Pages/Shared/Header/Header.jsx
@@ -1,11 +1,24 @@
-
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
 
 const Header = () => {
-    return (
-        <div>
-            <h1>This is Header</h1>
-        </div>
-    );
+  return (
+    <>
+      <Navbar className="mb-3" bg="primary" data-bs-theme="dark">
+        <Container>
+          <Navbar.Brand to='\'>CoursePlanet</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link to='\'>Home</Nav.Link>
+            <Nav.Link to='\courses'>Courses</Nav.Link>
+            <Nav.Link to='\pricing'>Pricing</Nav.Link>
+            <Nav.Link to='\about'>About Us</Nav.Link>
+            <Nav.Link to='\contact'>Contact Us</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+    </>
+  );
 };
 
 export default Header;
--- src/Pages/Shared/SocialMedia/SocialMedia.jsx
+++ src/Pages/Shared/SocialMedia/SocialMedia.jsx
@@ -1,11 +1,33 @@
-
+import { ButtonGroup, Button } from "react-bootstrap";
+import { FaGoogle, FaFacebook, FaTwitch, FaLinkedin, FaTwitter, FaInstagram } from "react-icons/fa";
+import ListGroup from "react-bootstrap/ListGroup";
 
 const SocialMedia = () => {
-    return (
-        <div>
-            <h1>All Social Media</h1>
-        </div>
-    );
+  return (
+    <div>
+      <ButtonGroup className="w-100" vertical>
+        <Button
+          className="mb-2"
+          variant="outline-primary"
+        >
+          {" "}
+          <FaGoogle></FaGoogle> LogIn with Google
+        </Button>
+        <Button variant="outline-dark">
+          {" "}
+          <FaLinkedin></FaLinkedin> LogIn with LinkedIn
+        </Button>
+      </ButtonGroup>
+      <h5 className="my-2">Find Us On </h5>
+      <ListGroup>
+        <ListGroup.Item className="mb-2"> <FaFacebook></FaFacebook> Facebook </ListGroup.Item>
+        <ListGroup.Item className="mb-2"> <FaTwitter></FaTwitter> Twitter</ListGroup.Item>
+        <ListGroup.Item className="mb-2"> <FaTwitch></FaTwitch> Twitch </ListGroup.Item>
+        <ListGroup.Item className="mb-2"> <FaInstagram></FaInstagram> Instagram</ListGroup.Item>
+        <ListGroup.Item className="mb-2">Click For More</ListGroup.Item>
+      </ListGroup>
+    </div>
+  );
 };
 
 export default SocialMedia;
